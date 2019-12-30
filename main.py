7bb35cfaf202f3e4da473dd134b95ef82aed92b5
Ihor <galileyforever@gmail.com>
2019-12-30 20:03:32 +0200

    new commands

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ bot = telebot.TeleBot('975958816:AAGLi7yZgMErf3EwgaapEwSkBgufKurlQnk')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    bot.send_message(message.chat.id, 'Привіт, що нового ?')
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text == 'Привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
-    elif message.text == 'Пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
+    if message.text == 'Привіт':
+        bot.send_message(message.chat.id, 'Привіт, ти мій творець ?')
+    elif message.text == 'бувай' or message.text == 'папа':
+        bot.send_message(message.chat.id, 'Бувай, папа, аліведерчі')
+    else:
+        bot.send_message(message.chat.id, 'Га, що, нашо ?')
 
 bot.polling()
